e347db1c3497e763454d459f8cd50ce2bde0c999
GratifyingCat <75364637+GratifyingCat@users.noreply.github.com>
2020-12-04 11:49:16 +0300

    Add files via upload
    Strategy method

---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Hello
+{
+    class Navigator
+    {
+        private RouteStrategy strategy;
+
+        public void BuildRoute(int a, int b)
+        {
+            this.strategy.BuildRoute(a, b);
+        }
+
+        public void SetStrategy(RouteStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+    }
+
+    interface RouteStrategy
+    {
+        Route BuildRoute(int a, int b);
+    }
+
+    class Route
+    {
+
+    }
+
+    class RoadStrategy : RouteStrategy
+    {
+        public Route BuildRoute(int a, int b)
+        {
+            System.Console.WriteLine("RoadStrategy");
+            return new Route();
+        }
+    }
+
+    public static void Main()
+    {
+        Navigator navigator = new Navigator();
+        navigator.SetStrategy(new RoadStrategy());
+        navigator.BuildRoute(1, 1);
+    }
+}
