c9ebb8464ce96ad929fe61b1214931045ebb93d4
GratifyingCat <75364637+GratifyingCat@users.noreply.github.com>
2020-12-04 11:44:35 +0300

    Factory method

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Hello
+{
+    class Creator
+    {
+        public void Send()
+        {
+            Console.WriteLine("Factory Method!");
+        }
+    }
+
+    class ConcreteCreator : Creator
+    {
+
+    }
+
+    class Product
+    {
+        public void CreateProduct()
+        {
+            Console.WriteLine("Product has been created!");
+        }
+    }
+
+    class ConcreteProduct : Product
+    {
+
+    }
+
+    public static void Main()
+    {
+        Creator cr = new Creator();
+        cr.Send();
+        ConcreteCreator ccr = new ConcreteCreator();
+        ccr.Send();
+        Product pr = new Product();
+        pr.CreateProduct();
+        ConcreteProduct cpr = new ConcreteProduct();
+        cpr.CreateProduct();
+    }
+}
